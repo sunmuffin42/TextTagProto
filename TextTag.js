25611e63fe4c2f45411e760a5a7e347d6d218882
Patrick Hall <pjh@umail.ucsb.edu>
2017-09-27 11:00:51 -0400

    use let not var. var is so last season.

--- TextTag.js
+++ TextTag.js
@@ -1,17 +1,17 @@
 // Create inputs for the header rows
 function makeHeaderInput(numHeaderRows){
-	for (var i = 1; i <= numHeaderRows; i++) {
+	for (let i = 1; i <= numHeaderRows; i++) {
 		document.querySelector('#row').insertAdjacentHTML('beforeend', `<td><input type="text" placeholder="Header Row ${i} name" name="headerDataNum${i}" id="headerData_${i}"> </td>`)
 	}
 	document.querySelector('#dataInfo').insertAdjacentHTML('beforeend', `<p></p><label for="dataRows">How many rows are there per line of text</label><input type="text" id="dataRows" placeholder="Number"><button name="submit" onclick="rowNames(document.querySelector('#dataRows').value)">Submit</button>`)
 
 }
 
-var a = 1
+let a = 1
 
 function makeHeaderAnnotations(atts){
 	document.querySelector('#data').insertAdjacentHTML('beforeend', `<ul id = "row_${a}"">`)
-	for (var i = 0; i < atts.length; i++) {
+	for (let i = 0; i < atts.length; i++) {
 		 document.querySelector('#data').insertAdjacentHTML('beforeend', `<li><input type="text" placeholder="${atts[i]}" name="${i}" id="data_${i}"> </li>`)
 	}
 	document.querySelector('#data').insertAdjacentHTML('beforeend', `</ul>`)
@@ -20,7 +20,7 @@ function makeHeaderAnnotations(atts){
 
 function rowNames(num_rows){
 	document.querySelector('#dataInfo').insertAdjacentHTML('beforeend', `<table id="namesOfRows"><tr id="onlyRow">`)
-	for (var i = 1; i <= num_rows; i++){
+	for (let i = 1; i <= num_rows; i++){
 		document.querySelector('#onlyRow').insertAdjacentHTML('beforeend', `<td><input type="text" placeholder="Name of Row" id="rowName${i}"</td>`)
 	}
 	document.querySelector('#dataInfo').insertAdjacentHTML('beforeend', `</tr></table>`)
@@ -28,23 +28,23 @@ function rowNames(num_rows){
 }
 
 function annotateText(){
-	var header = []
-	for (var i = 1; i <= document.querySelector('#headers').value; i++) {
+	let header = []
+	for (let i = 1; i <= document.querySelector('#headers').value; i++) {
 		header.push(document.querySelector(`#headerData_${i}`).value)
 	}
 
         let splitUp = document.querySelector('#in').value.split('\n')
         splitUp = splitUp.filter(x => x.trim())
 
-	var annotations = []
-	for (var i = 1; i <= document.querySelector('#dataRows').value; i++) {
+	let annotations = []
+	for (let i = 1; i <= document.querySelector('#dataRows').value; i++) {
 		annotations.push(document.querySelector(`#rowName${i}`).value)
 	}
-	for (var i = 0; i < header.length; i++) {
+	for (let i = 0; i < header.length; i++) {
 		document.querySelector('#out').value += '\\' + header[i] + ' ' + splitUp[i] + '\n'
 	}
-	var b = 0
-	for (var i = header.length; i < splitUp.length; i++) {
+	let b = 0
+	for (let i = header.length; i < splitUp.length; i++) {
 		document.querySelector('#out').value += '\\' + annotations[b%(annotations.length)] + ' ' + splitUp[i] + '\n'
 		b = b + 1
 	}
